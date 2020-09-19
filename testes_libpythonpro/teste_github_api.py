dbd58aa716ae53dfde3d56dd54482b1c54d113ce
wfoschiera <wfoschiera@gmail.com>
2020-09-19 16:45:05 -0400

    ajustes para atender PEP8

--- testes_libpythonpro/teste_github_api.py
+++ testes_libpythonpro/teste_github_api.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
 from libpythonpro.github_api import buscar_avatar
 
+
 class TesteGithubApi(TestCase):
     def teste_url_valida(self):
         usuario = 'wfoschiera'
         resposta = 'https://avatars2.githubusercontent.com/u/7304236?v=4'
         self.assertEqual(buscar_avatar(usuario), resposta)
-
