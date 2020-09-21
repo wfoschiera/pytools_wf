24c97fa136389bbeb76ae2463acae1f3930e2970
wfoschiera <wfoschiera@gmail.com>
2020-09-21 19:57:03 -0400

    Teste validação Travis CI

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -9,4 +9,5 @@ def buscar_avatar(usuario):
     """
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
-    return resp.json()['avatar_url']
+    json = resp.json()
+    return json['avatar_url']
