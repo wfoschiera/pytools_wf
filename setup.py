aa53d91dc56d1ccaa2ed9dade23d5b6a8491d225
wfoschiera <wfoschiera@gmail.com>
2020-09-21 10:25:52 -0400

    corrigido nome da lib para instalação via pip

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpythonpro_wf"
+PACKAGE = "libpythonpro"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "William Foschiera"
