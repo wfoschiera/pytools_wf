cd2697f27c66a1eb0d494d70cff846502bf21fb1
wfoschiera <wfoschiera@gmail.com>
2020-09-30 16:21:54 -0400

    Alterado nome do pacote para libpythonpro_wf, para corrigir conflito com nome original

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpythonpro"
+PACKAGE = "libpythonpro_wf"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "William Foschiera"
