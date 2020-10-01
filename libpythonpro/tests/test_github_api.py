1aeb0753423a5c932a785c89dfe0b9df23a909e1
wfoschiera <wfoschiera@gmail.com>
2020-10-01 07:50:55 -0400

    developing githup_api

--- libpythonpro/tests/test_github_api.py
+++
@@ -1,7 +0,0 @@
-from libpythonpro.github_api import buscar_avatar
-
-
-def test_url_valida():
-    usuario = 'wfoschiera'
-    resposta = 'https://avatars2.githubusercontent.com/u/7304236?v=4'
-    assert buscar_avatar(usuario) == resposta
