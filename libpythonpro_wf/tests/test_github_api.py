56cbde4860bee08f9959b4ae680f1e6f1702e6b1
wfoschiera <wfoschiera@gmail.com>
2020-10-01 08:20:39 -0400

    pep8 fix

--- libpythonpro_wf/tests/test_github_api.py
+++ libpythonpro_wf/tests/test_github_api.py
@@ -4,4 +4,4 @@ from libpythonpro_wf.github_api import buscar_avatar
 def test_url_valida():
     url = buscar_avatar('wfoschiera')
     resposta = 'https://avatars2.githubusercontent.com/u/7304236?v=4'
-    assert  url == resposta
+    assert url == resposta
