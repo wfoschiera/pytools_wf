c0dbe409575938f4b5b5729cc262a8c7f338353e
wfoschiera <wfoschiera@gmail.com>
2020-10-04 15:00:12 -0400

    implementados melhores testes para múltiplos usuários

--- libpythonpro_wf/spam/test_usuarios.py
+++ libpythonpro_wf/spam/test_usuarios.py
@@ -1,9 +1,25 @@
-class Conexao(object):
-    def gerar_sessao(self):
-        pass
+from libpythonpro_wf.spam.db import Conexao
+from libpythonpro_wf.spam.modelos import Usuario
 
 
 def test_salvar_usuario():
     conexao = Conexao()
     sessao = conexao.gerar_sessao()
-    return sessao
+    usuario = Usuario(nome='Renzo')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
+
+
+def test_salvar_usuario():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuarios = [Usuario(nome='Renzo'), Usuario(nome='Renzo')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
