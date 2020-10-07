36c90075ad6b44ec02ed7b5ac591b41f6270b86d
wfoschiera <wfoschiera@gmail.com>
2020-10-07 16:50:18 -0400

    added tests for github_api using fixture and mock

--- libpythonpro_wf/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro_wf/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,8 +1,8 @@
 import pytest
 
-from libpythonpro_wf.spam.enviador_de_email import Enviador
 from libpythonpro_wf.spam.main import EnviadorDeSpam
 from libpythonpro_wf.spam.modelos import Usuario
+from unittest.mock import Mock
 
 
 @pytest.mark.parametrize(
@@ -13,18 +13,37 @@ from libpythonpro_wf.spam.modelos import Usuario
             Usuario(nome='Luciano', email='renzo@python.pro.br')
         ],
         [
-        Usuario(nome='Renzo', email='renzo@python.pro.br')
+            Usuario(nome='Renzo', email='renzo@python.pro.br')
         ]
     ]
 )
 def test_qde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'renzo@python.pro.br',
         'Curso Python Pro',
         'Confira os módulos fantásticos'
     )
-    assert len(usuarios) == enviador.qtd_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Renzo', email='renzo@python.pro.br')
+    sessao.salvar(usuario)
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'luciano@python.pro.br',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
+
+    enviador.enviar.assert_called_once_with(
+        'luciano@python.pro.br',
+        'renzo@python.pro.br',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
